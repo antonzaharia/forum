ec994cca72c2a3b003b9e96dc0cad059cd03f9f2
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-13 16:19:49 +0000

    Generated categories for discussions

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,12 +1,13 @@
 class Discussion < ApplicationRecord
+  belongs_to :category, counter_cache: true, touch: true
   belongs_to :user, default: -> { Current.user }
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true
 
-  after_create_commit -> { broadcast_prepend_to 'discussions' }
-  after_update_commit -> { broadcast_replace_to 'discussions' }
-  after_destroy_commit -> { broadcast_remove_to 'discussions' }
+  after_create_commit -> { broadcast_prepend_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
 
   accepts_nested_attributes_for :posts
 
