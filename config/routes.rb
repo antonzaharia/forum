d6671c495f1c96ff9a08e2691ca65641686ab037
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-13 11:22:50 +0000

    Edit discussion action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "main#index"
 
-  resources :discussions, only: [:index, :create, :new]
+  resources :discussions, only: [:index, :new, :create, :edit, :update]
 end
