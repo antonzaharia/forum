2d00b255018a67e11d4c28a7e420dfa4983ff502
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-14 12:09:45 +0000

    Display notifications in UI and mark as read

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,15 @@ Rails.application.routes.draw do
   resources :discussions do
     resources :posts, only: [:show, :create, :edit, :update, :destroy], module: :discussions
     collection do
-      get 'category/:id', to: "categories/discussions#index", as: :category
+      get "category/:id", to: "categories/discussions#index", as: :category
     end
 
     resources :notifications, only: :create, module: :discussions
+  end
+
+  resources :notifications, only: :index do
+    collection do
+      post "/mark_as_read", to: "notifications#read_all", as: :read
+    end
   end
 end
